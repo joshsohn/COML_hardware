a2500073c1ff1550938a0334c8c2b06837bc3b71
Josh Sohn <jcsohn@mit.edu>
2024-04-10 23:39:57 -0400

    pid controller

--- utils.py
+++ utils.py
@@ -101,6 +101,15 @@ def quaternion_to_rotation_matrix(Q):
     return rot_matrix
 
 
+def quaternion_multiply(quaternion0, quaternion1):
+    w0, x0, y0, z0 = quaternion0
+    w1, x1, y1, z1 = quaternion1
+    return jnp.array([-x1 * x0 - y1 * y0 - z1 * z0 + w1 * w0,
+                     x1 * w0 + y1 * z0 - z1 * y0 + w1 * x0,
+                     -x1 * z0 + y1 * w0 + z1 * x0 + w1 * y0,
+                     x1 * y0 - y1 * x0 + z1 * w0 + w1 * z0])
+
+
 def hat(in_vec):
     out_mat = jnp.array([[0, -in_vec[2], in_vec[1]],
                         [in_vec[2], 0, -in_vec[0]],

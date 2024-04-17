7941d64a276921c0ef3752f3c4283539bebc25b0
Josh Sohn <jcsohn@mit.edu>
2024-04-16 23:35:04 -0400

    hardware_15,16.pkl

--- train_reg_P.sh
+++ train_reg_P.sh
@@ -8,11 +8,10 @@ do
         do
             for p_freq in 2000
             do
-                for reg_P in 1e-1 1 10 100
-                # for reg_P in 500 1000 5000
+                for reg_P in 1e-1 5e-1 1
                 do
-                    echo "seed = $seed, M = $M, pnorm_init = $p, p_freq = $p_freq, meta_epochs = 1000, reg_P = $reg_P output_dir = hardware_ubuntu_smallP_reg_P_$reg_P"
-                    python3 train_Lp_varfreq.py $seed $M --pnorm_init $p --p_freq $p_freq --meta_epochs 1000 --reg_P $reg_P --output_dir "hardware_ubuntu_smallP_reg_P_$reg_P"
+                    echo "seed = $seed, M = $M, pnorm_init = $p, p_freq = $p_freq, meta_epochs = 1000, reg_P = $reg_P output_dir = hardware_z_up_$reg_P"
+                    python3 train_z_up.py $seed $M --pnorm_init $p --p_freq $p_freq --meta_epochs 1000 --reg_P $reg_P --output_dir "hardware_z_up_$reg_P"
                 done
             done
         done

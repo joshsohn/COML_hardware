e30564cbf711ce7ad0dbe466c03bdac61908587c
Josh Sohn <jcsohn@mit.edu>
2024-04-17 00:24:37 -0400

    z_up_1e-1

--- train_reg_P.sh
+++ train_reg_P.sh
@@ -10,8 +10,8 @@ do
             do
                 for reg_P in 1e-1 5e-1 1
                 do
-                    echo "seed = $seed, M = $M, pnorm_init = $p, p_freq = $p_freq, meta_epochs = 1000, reg_P = $reg_P output_dir = hardware_z_up_$reg_P"
-                    python3 train_z_up.py $seed $M --pnorm_init $p --p_freq $p_freq --meta_epochs 1000 --reg_P $reg_P --output_dir "hardware_z_up_$reg_P"
+                    echo "seed = $seed, M = $M, pnorm_init = $p, p_freq = $p_freq, meta_epochs = 1000, reg_P = $reg_P output_dir = z_up_reg_P_$reg_P"
+                    python3 train_z_up.py $seed $M --pnorm_init $p --p_freq $p_freq --meta_epochs 1000 --reg_P $reg_P --output_dir "z_up_reg_P_$reg_P"
                 done
             done
         done

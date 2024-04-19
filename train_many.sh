799b013346e3c2efa417eb86ab1f0da830455640
Josh Sohn <jcsohn@mit.edu>
2024-04-19 19:23:43 -0400

    fix test_kR.ipynb

--- train_many.sh
+++ train_many.sh
@@ -13,7 +13,7 @@ do
             do
                 for reg_P in 1
                     do
-                    for reg_k_R in 1e-4 1e-2 1e-1 1
+                    for reg_k_R in 1e-5 1e-4 1e-2 1e-1
                     do
                         echo "seed = $seed, M = $M, pnorm_init = $p, p_freq = $p_freq, meta_epochs = 1000, reg_P = $reg_P reg_k_R=$reg_k_R output_dir = reg_P_${reg_P}_reg_k_R_${reg_k_R}"
                         sbatch train_single.sh $seed $M --pnorm_init $p --p_freq $p_freq --meta_epochs 1000 --reg_P $reg_P --reg_k_R $reg_k_R --output_dir "reg_P_${reg_P}_reg_k_R_${reg_k_R}"

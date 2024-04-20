403a2cd5a598cabed79cea29cc4f17689bf4cfbc
Josh Sohn <jcsohn@mit.edu>
2024-04-20 06:28:46 -0400

    small edit

--- train_many.sh
+++ train_many.sh
@@ -15,7 +15,7 @@ do
                     do
                     for reg_k_R in 2e-3
                     do
-                        for k_R_scale in 2 3 4
+                        for k_R_scale in 1.5 2 3 4
                         do
                             echo "seed = $seed, M = $M, pnorm_init = $p, p_freq = $p_freq, meta_epochs = 1000, reg_P = $reg_P reg_k_R = $reg_k_R k_R_scale = $k_R_scale output_dir = reg_P_${reg_P}_reg_k_R_${reg_k_R}_k_R_scale_${k_R_scale}"
                             sbatch train_single.sh $seed $M --pnorm_init $p --p_freq $p_freq --meta_epochs 1000 --reg_P $reg_P --reg_k_R $reg_k_R --k_R_scale $k_R_scale --output_dir "reg_P_${reg_P}_reg_k_R_${reg_k_R}_k_R_scale_${k_R_scale}"

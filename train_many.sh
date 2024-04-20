6998ce9a934425b987d37be23852b495dec5b75f
Josh Sohn <jcsohn@mit.edu>
2024-04-20 15:45:41 -0400

    multiple values of reg_P for k_R_scale

--- train_many.sh
+++ train_many.sh
@@ -11,11 +11,11 @@ do
         do
             for p_freq in 2000
             do
-                for reg_P in 2e-3
+                for reg_P in 1e-2 1e-1 5e-1 1
                     do
-                    for reg_k_R in 2e-3
+                    for reg_k_R in 2e-3 
                     do
-                        for k_R_scale in 1.5 2 3 4
+                        for k_R_scale in 3
                         do
                             echo "seed = $seed, M = $M, pnorm_init = $p, p_freq = $p_freq, meta_epochs = 1000, reg_P = $reg_P reg_k_R = $reg_k_R k_R_scale = $k_R_scale output_dir = reg_P_${reg_P}_reg_k_R_${reg_k_R}_k_R_scale_${k_R_scale}"
                             sbatch train_single.sh $seed $M --pnorm_init $p --p_freq $p_freq --meta_epochs 1000 --reg_P $reg_P --reg_k_R $reg_k_R --k_R_scale $k_R_scale --output_dir "reg_P_${reg_P}_reg_k_R_${reg_k_R}_k_R_scale_${k_R_scale}"

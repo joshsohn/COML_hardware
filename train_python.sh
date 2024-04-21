917e98e8fc2e4dd70de07d34d4158dd071b5bdde
Josh Sohn <jcsohn@mit.edu>
2024-04-21 15:08:55 -0400

    4000 epochs

--- train_python.sh
+++ train_python.sh
@@ -10,12 +10,15 @@ do
             do
                 for reg_P in 2e-3
                     do
-                    for reg_k_R in 2e-3
+                    for reg_k_R in 2e-3 
                     do
                         for k_R_scale in 1
                         do
-                            echo "seed = $seed, M = $M, pnorm_init = $p, p_freq = $p_freq, meta_epochs = 1000, reg_P = $reg_P reg_k_R = $reg_k_R k_R_scale = $k_R_scale output_dir = reg_P_${reg_P}_reg_k_R_${reg_k_R}_k_R_scale_${k_R_scale}"
-                            python3 train_z_up_kR.py $seed $M --pnorm_init $p --p_freq $p_freq --meta_epochs 1000 --reg_P $reg_P --reg_k_R $reg_k_R --k_R_scale $k_R_scale --output_dir "reg_P_${reg_P}_reg_k_R_${reg_k_R}_k_R_scale_${k_R_scale}"
+                            for k_R_z in 1.26
+                            do
+                                echo "seed = $seed, M = $M, pnorm_init = $p, p_freq = $p_freq, meta_epochs = 4000, reg_P = $reg_P reg_k_R = $reg_k_R k_R_scale = $k_R_scale k_R_z = $k_R_z output_dir = reg_P_${reg_P}_reg_k_R_${reg_k_R}_k_R_z_${k_R_z}_epochs_4000"
+                                python3 train_z_up_kR1.py $seed $M --pnorm_init $p --p_freq $p_freq --meta_epochs 4000 --reg_P $reg_P --reg_k_R $reg_k_R --k_R_scale $k_R_scale --k_R_z $k_R_z --output_dir "reg_P_${reg_P}_reg_k_R_${reg_k_R}_k_R_z_${k_R_z}_epochs_4000"
+                            done
                         done
                     done
                 done

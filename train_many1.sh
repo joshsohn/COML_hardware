45ff7f03bb5f92ab35ef77831da3b3f3e1607f1c
Josh Sohn <jcsohn@mit.edu>
2024-04-22 18:29:16 -0400

    reg_P = 5e-1

--- train_many1.sh
+++ train_many1.sh
@@ -11,16 +11,16 @@ do
         do
             for p_freq in 2000
             do
-                for reg_P in 2e-3
+                for reg_P in 5e-1
                     do
                     for reg_k_R in 0
                     do
-                        for k_R_scale in 1
+                        for k_R_scale in 1 1.5 2 2.5
                         do
                             for k_R_z in 1.26
                             do
-                                echo "seed = $seed, M = $M, pnorm_init = $p, p_freq = $p_freq, meta_epochs = 4000, reg_P = $reg_P reg_k_R = $reg_k_R k_R_scale = $k_R_scale k_R_z = $k_R_z output_dir = reg_P_${reg_P}_reg_k_R_${reg_k_R}_epochs_4000"
-                                sbatch train_single.sh $seed $M --pnorm_init $p --p_freq $p_freq --meta_epochs 4000 --reg_P $reg_P --reg_k_R $reg_k_R --k_R_scale $k_R_scale --k_R_z $k_R_z --output_dir "reg_P_${reg_P}_reg_k_R_${reg_k_R}_epochs_4000"
+                                echo "seed = $seed, M = $M, pnorm_init = $p, p_freq = $p_freq, meta_epochs = 1000, reg_P = $reg_P reg_k_R = $reg_k_R k_R_scale = $k_R_scale k_R_z = $k_R_z output_dir = reg_P_${reg_P}_reg_k_R_${reg_k_R}"
+                                sbatch train_single.sh $seed $M --pnorm_init $p --p_freq $p_freq --meta_epochs 1000 --reg_P $reg_P --reg_k_R $reg_k_R --k_R_scale $k_R_scale --k_R_z $k_R_z --output_dir "reg_P_${reg_P}_reg_k_R_${reg_k_R}"
                             done
                         done
                     done
